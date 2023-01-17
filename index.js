2886dc32eb6c926db2da3a473eaa674b6529dc96
Baek Yeiwon <yeiwonxd@gmail.com>
2023-01-17 13:17:41 +0900

    node

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.get("/", function (req, res) {
+  res.render("index");
+});
+app.get("/vision", function (req, res) {
+  res.render("vision", { subTitle: "vision" });
+});
+app.get("/location", function (req, res) {
+  res.render("location", { subTitle: "location" });
+});
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "html/index.html"));
+  res.sendFile(path.join(__dirname, "index"));
 });
 app.listen(3000, function () {
   console.log("3000번에서 서버 대기중");
